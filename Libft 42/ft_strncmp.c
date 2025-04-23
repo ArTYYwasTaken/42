5c4b2055d0a69fb45f0bf940f90e45a28fd98d61
Kelle <kellejms.42@gmail.com>
2025-04-23 22:48:15 +0100

    Up to date

--- Libft 42/ft_strncmp.c
+++ Libft 42/ft_strncmp.c
@@ -30,9 +30,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while ((s1[i] || s2[i]) && (i < n))
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
 /* 
 // TEST CODE
