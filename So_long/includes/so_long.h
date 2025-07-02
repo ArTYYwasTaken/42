4fdc9b65bc7d9bc81e96158ae24471890bc539df
Kelle <kellejms.42@gmail.com>
2025-07-02 18:44:38 +0100

    mid so long

---
+++ So_long/includes/so_long.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemontei <kemontei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 16:15:57 by kemontei          #+#    #+#             */
+/*   Updated: 2025/07/02 18:44:30 by kemontei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../libs/Libft/includes/libft.h"
+# include <mlx.h>
+
+typedef struct game
+{
+	t_map *map;
+	t_player *player;
+	char *temp;
+	int col;
+	int exit;
+	
+} t_game;
+
+typedef struct map_stuff
+{
+	char **grid;
+	int height;
+	int width;
+
+} t_map;
+
+typedef struct player_stuff
+{
+	char **player;
+	int x;
+	int y;
+
+} t_player;
+
+void gamestart (t_game *game, char *mapfile);
+
+#endif
